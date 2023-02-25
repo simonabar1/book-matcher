a8f01287a9b024fbbd6067199294f4eda5601666
Simona Barone <simona_barone@hotmail.com>
2023-02-25 12:15:36 +0100

    added users controller, routes, show and index pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :city, :gender, :children, :habitation, :hobbies, :description, :email, :password, :photo)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
